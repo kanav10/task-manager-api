14348e255522e8b80733dd4645d1db70c4604a2a
Kanav Malik <kanav@renebyte.xyz>
2020-04-23 16:17:18 +0530

    api-middleware

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const bodyParser = require('body-parser');
 //Load middleware
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 //Mongoose models
 const {List, Task} = require('./db/models');
 
